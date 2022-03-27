8da9446f352f2f95783e1c1a62542473337261c0
swillrich5 <swillrich@gmail.com>
2022-03-27 13:47:55 -0500

    bootstrap and complete base.html

--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Book
+
+from .models import Book, Review
+from .utils import average_rating
 
 # Create your views here.
-def index(request):
-    name = request.GET.get("name") or "world"
-    return render(request, 'base.html', {"name": name})
 
-def search(request):
-    search = request.GET.get("search") or "No Search Term Entered"
-    return render(request, 'search_result.html', {"search": search})
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({'book': book, 
+                            'book_rating': book_rating,
+                            'number_of_reviews': number_of_reviews})
+    context = {
+            'book_list': book_list
+    }
+    return render(request, 'reviews/book_list.html', context)
 
 def welcome_view(request):
-    message = f"<html><h1>Welcome to Bookr!</h1> <p>{Book.objects.count()} books and counting!</p></html>"
-    return HttpResponse(message)
+    return render(request, 'base.html')
